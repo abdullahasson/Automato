ed38c31343a8ea853653bdf979fdab5361309ee4
Abdullah <bh532997@gmail.com>
2024-05-15 03:16:38 +0300

    responsive mobile

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
                 <h1 className="text-[4.7rem] leading-[1.6] max-[500px]:text-7xl max-[500px]:leading-[1.4] font-bold">
                     منوفر وقتك الضائع على الأشياء الغير مهمة
                 </h1>
-                <p className="text-[2rem] leading-[1.6] mt-[1.1rem] mb-[1.3rem] max-[500px]:text-[18px] max-[500px]:my-[20px]">
+                <p className="text-[2rem] leading-[1.6] mt-[1.1rem] mb-[1.3rem] max-[500px]:text-[18px] max-[500px]:my-[20px] max-[482px]:px-[28px]">
                     حقق أفضل استثمار لوقت الموارد البشرية بشركتك عن طريق أتمتة المهمات الروتينية المملة من قبل مجموعة متخصصين في أتمتة<br /> العمليات السوفتوير
                 </p>
                 <Button>التواصل معنا</Button>
@@ -22,7 +22,7 @@ const Home = () => {
                 <img className="inline absolute rotate-90 translate-x-5 left-0 -bottom-[18px]" src={Leaft} alt />
             </div>
             <div className="right">
-                <img className="inline absolute right-0 bottom-0 max-[767px]:w-[215px] max-[767px]:top-[-9px] max-[767px]:-rotate-90 max-[767px]:right-4 max-[767px]:bottom-auto" src={Leaft} alt />
+                <img className="inline absolute right-0 bottom-0 max-[767px]:w-[215px] max-[767px]:top-[-9px] max-[767px]:-rotate-90 max-[767px]:right-4 max-[767px]:bottom-auto max-[482px]:-top-[67px] max-[482px]:z-[-1]" src={Leaft} alt />
             </div>
         </section>
     )
